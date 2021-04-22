a1465d455b102ad996f3b38e164cc40e8c724ec1
naufalfidyanramadhan <54935087+naufalfidyanramadhan@users.noreply.github.com>
2021-04-22 12:59:47 +0700

    Add files via upload

---
+++ Modul 10/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBzaMABFa3yW6Qu-NHHiRwiUvfTzOqWSkQ",
+  authDomain: "fir-pbf.firebaseapp.com",
+  projectId: "fir-pbf",
+  storageBucket: "fir-pbf.appspot.com",
+  messagingSenderId: "555437055816",
+  appId: "1:555437055816:web:b77911c23dea2069a2dd7d",
+  measurementId: "G-GF4YRD7Y7P"
+};
